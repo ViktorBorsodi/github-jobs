9a9152b153d148893dd6586bdd8067f781d18064
Viktor Borsodi <borsodiviktor@gmail.com>
2020-02-15 15:58:13 +0100

    Updates

---
+++ precache-manifest.329a00e2c524a001d77feeaecf51f3d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a29c02981201a8176edc260b810ec19",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "c5f9bb4e82a4a677e130",
+    "url": "/github-jobs/static/css/main.4f0d80e1.chunk.css"
+  },
+  {
+    "revision": "8f396cfbfd16990fa521",
+    "url": "/github-jobs/static/js/2.672f7214.chunk.js"
+  },
+  {
+    "revision": "c5f9bb4e82a4a677e130",
+    "url": "/github-jobs/static/js/main.ce9c1d66.chunk.js"
+  },
+  {
+    "revision": "de9736956e5cfce453cd",
+    "url": "/github-jobs/static/js/runtime-main.ef5e73cd.js"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/github-jobs/static/media/back.6c9efe0c.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/github-jobs/static/media/next.be237328.svg"
+  }
+]);
